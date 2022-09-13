6cfbb5088da633625672bd1b4d6b66061b7949fe
Impenia <akpaetuki@gmail.com>
2022-09-13 14:04:50 -0700

    Absolute Integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,13 +4,13 @@
 * _abs - Compute absolute value of an integer
 *@i: integer to be checked
 *
-* Return: absolute int 
+* Return: absolute int
 */
 
 int _abs(int i)
 {
 
-i = (i >= 0) ? i : (-1*i);
+i = (i >= 0) ? i : (-1 * i);
 
 return (i);
 
