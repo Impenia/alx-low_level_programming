b0e92ac0dae21009f80a5596f7c0c8055ea2e75d
Impenia <akpaetuki@gmail.com>
2022-09-13 06:50:12 -0700

    _Putchar

---
+++ 0x02-functions_nested_loops/1-putchar.c
@@ -0,0 +1,24 @@
+#include "./main.h"
+
+/**
+* main - Print _putchar
+*
+* Return: Always (0) Success
+*/
+
+int main(void)
+{
+
+int i;
+char a[] = "_putchar";
+
+for (i = 0; i < 8; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+
+return (0);
+}
+
+
