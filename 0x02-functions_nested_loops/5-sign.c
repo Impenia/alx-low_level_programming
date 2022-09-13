10340bed6d5cccacc9c699247a7a272fb7f2330d
Impenia <akpaetuki@gmail.com>
2022-09-13 13:35:49 -0700

    islower

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_sign - print sign of a number
+* @n: number to be checked
+*
+* Return: 1 if n > 0. Return 0 if n = 0. Return -1 if n < 0.
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+  return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+_purtchar('\n');
+}
