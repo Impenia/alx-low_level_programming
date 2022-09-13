2130e13a18e311bf1bf99a7f0f164d604f633bfa
Impenia <akpaetuki@gmail.com>
2022-09-13 16:13:02 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#inclue "main.h"
+
+/**
+* add - Add two integers and return result
+* @a: first integer
+* @b: second integer
+*
+* Return: (a + b);
+*/
+
+int add(int a, int b)
+{
+
+int sum = a + b;
+
+return (sum);
+
+_putchar('\n');
+
+}
