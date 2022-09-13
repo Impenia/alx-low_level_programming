7a791fc74929d4eef74986b9d63f3bf728d325f0
Impenia <akpaetuki@gmail.com>
2022-09-13 16:49:43 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,17 +13,22 @@ void print_to_98(int n)
 
 int i;
 
-for (i = n; i <= 98; i++)
+if (n < 98)
 {
-printf("%i", i);
-if (i < 98)
+for (i = n; i < 98; i++)
 {
-printf(", ");
+printf("%i, ", i);
 }
-else
-{
-printf("\n");
 }
+else if (n > 98)
+{
+for (i = n; i > 98; i--;)
+{
+printf("%i, ", i);
+}
+}
+
+ printf("%i\n", i);
 
 }
 
