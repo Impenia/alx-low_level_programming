ced6b2e39675fe5f3c3246dbe0414c791f4a62ce
Impenia <akpaetuki@gmail.com>
2022-09-13 16:20:05 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_to_98 - Print all natural numbers from n to 98
+* @n: number start
+*
+* Return: (1).
+*/
+
+void print_to_98(int n)
+{
+
+int i;
+
+for (i = n; i <= 98; i++)
+{
+printf("%i\n", i);
+}
