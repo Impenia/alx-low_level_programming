3a41c4241480dac480a45d2dec0bbb60eda36e2f
Impenia <akpaetuki@gmail.com>
2022-09-13 16:04:03 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 
 int a, b, c, d;
-  
+
 for (a = 48; a <= 50; a++)
-    
+
 {
 
 for (b = 48; b <= 57; b++)
@@ -20,27 +20,22 @@ for (b = 48; b <= 57; b++)
 {  
 
 for (c = 48; c <= 53; c++)
-	    
+
 {
-	      
+
 for (d = 48; d <= 57; d++)
-		
+
 {
-		  
+
 if (a >= 50 && b >= 52)
-		    
+
 break;
-		  
+
 _putchar(a);
-		  
 _putchar(b);
-		  
 _putchar(58);
-		  
 _putchar(c);
-		  
 _putchar(d);
-		  
 _putchar('\n');
 		  
 }
