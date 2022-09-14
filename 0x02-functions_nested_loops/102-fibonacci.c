fab2e17ebac9d4d57aa016e1699ec2b5ea616a62
Impenia <akpaetuki@gmail.com>
2022-09-13 18:25:06 -0700

    Fibonacci 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - printfirst 50 fibonscci sequence starting with 1 & 2.
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+
+int num1 = 1;
+int num2 = 2;
+int nextnum = num1 + num2;
+
+printf("%i, %i, %i", num1, num2, nextnum);
+
+for (int i = 4; i <= 50; i++)
+{
+num1 = num2;
+num2 = nextnum;
+nextnum = num1 + num2;
+ 
+printf(", %i", nextnum);
+}
+
+printf("\n");
