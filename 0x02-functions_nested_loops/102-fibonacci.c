6b9e2872a634a9fdf5ab9d0230ec9098d0132e5e
Impenia <akpaetuki@gmail.com>
2022-09-14 04:19:24 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,17 +16,17 @@ int64_t num1 = 1;
 int64_t num2 = 2;
 int64_t nextnum = num1 + num2;
 
-printf("%" PRId64 "n", num1);
-printf(", %" PRId64 "n", num2);
-printf(", %" PRId64 "n", nextnum);
+printf("%" PRId64 "", num1);
+printf(", %" PRId64 "", num2);
+printf(", %" PRId64 "", nextnum);
 
 for (i = 4; i <= 50; i++)
 {
-  num1 = num2;
-  num2 = nextnum;
-  nextnum = num1 + num2;
+num1 = num2;
+num2 = nextnum;
+nextnum = num1 + num2;
 
-  printf(", %" PRId64 "n", nextnum);
+printf(", %" PRId64 "", nextnum);
 }
 
 printf("\n");
