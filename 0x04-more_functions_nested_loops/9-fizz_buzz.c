8b7640448468181f05bc7aa91e67156536bafdbf
Impenia <akpaetuki@gmail.com>
2022-09-15 14:47:52 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,15 +8,15 @@
 */
 
 int main(void)
-
 {
+
 int i;
 
 for (i = 1; i < 100; i++)
-
+{
 if (i % 15 == 0)
 {
-fprintf("FizzBuzz ");
+printf("FizzBuzz ");
 }
 else if (i % 5 == 0)
 {
@@ -36,4 +36,5 @@ printf("\n");
 
 return (0);
 
+}
 }
