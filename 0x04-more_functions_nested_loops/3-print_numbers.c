9c603ff6e4228831cc65fee6beb5f0c213e0743e
Impenia <akpaetuki@gmail.com>
2022-09-15 10:31:50 -0700

    Print numbers 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,13 +7,14 @@
 void print_numbers(void)
 {
 
-int i = 0;
+  char c;
+  
 
-while (i < = 9)
-{
-_putchar(c);
-i++;
-
-_putchar('\n');
+  
+  for (c = '0'; c <= '9'; c++)
+    
+    _putchar(c);
+  
+  _putchar('\n');
 
 }
