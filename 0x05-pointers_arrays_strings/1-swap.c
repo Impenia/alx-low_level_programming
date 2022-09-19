e93cebba1707d8a046695c3a2bf9b5f855f8abd7
Impenia <akpaetuki@gmail.com>
2022-09-19 06:34:02 -0700

    Pointer Reset

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* swap_int - a function to swap two integers
+* @a: variable to be swapped
+* @b: variable to be swapped
+*
+* Return: Void
+*/
+
+void swap_int(int *a, int *b)
+{
+
+int a;
+int b;
+*a = b;
+*b = a;
+
+}
