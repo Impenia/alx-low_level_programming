8a8fc278dd8fd81cb4789283bae87567ecbd779e
Impenia <akpaetuki@gmail.com>
2022-09-19 09:54:29 -0700

    Atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,69 +2,47 @@
 #include <limits.h>
 #include "main.h"
 
+/**
+* _atoi - function to converts a string to integer
+* @s: strint to be converted
+*
+* Return: integer converted
+*/
 
 int _atoi(char* s)
 {
 
     int sign = 1, base = 0, i = 0;
-
     
     /* if whitespaces then ignore.*/
-
     while (s[i] == ' ')
-
     {
-
         i++;
-
     }
-
-     
 
     /* sign of number */
-
     if (s[i] == '-' || s[i] == '+')
-
     {
-
         sign = 1 - 2 * (s[i++] == '-');
-
     }
 
-   
-
     /* checking for valid input */
-
     while (s[i] >= '0' && s[i] <= '9')
-
     {
 
         /* handling overflow test case */
-
-        if (base > INT_MAX / 10
-
-            || (base == INT_MAX / 10
-
-            && s[i] - '0' > 7))
-
+        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7))
         {
-
             if (sign == 1)
-
                 return INT_MAX;
-
             else
-
                 return INT_MIN;
-
         }
 
         base = 10 * base + (s[i++] - '0');
-
     }
 
     return base * sign;
-
 }
 
  
