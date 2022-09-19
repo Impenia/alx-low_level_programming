23e6e4b3d5727e313de110fa8e5ff6cfa2d38b99
Impenia <akpaetuki@gmail.com>
2022-09-19 12:47:25 -0700

    Print first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,37 +1,50 @@
-#include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
+#define LARGEST 10000000000
 
 /**
- * main - printfirst 98 fibonacci sequence starting with 1 & 2.
- *
- * Return: (0) Success
- */
+* main - Find and print the first 98 fibonacci numbers
+* Return: 0 (Success).
+*/
 
 int main(void)
 {
 
-int i;
-int64_t num1 = 1;
-int64_t num2 = 2;
-int64_t nextnum = num1 + num2;
+	int i;
+	uint64_t num1 = 0, num11 = 1;
+	uint64_t num2 = 0, num22 = 2;
+	uint64_t nonda1, nonda2, nonda3;
 
-printf("%" PRId64 "", num1);
-printf(", %" PRId64 "", num2);
+	printf("%" PRId64 ", ", num11);
+	printf("%" PRId64 ", ", num22);
 
-for (i = 3; i <= 98; i++)
-{
-printf(", %" PRId64 "", nextnum);
-num1 = num2;
-num2 = nextnum;
-nextnum = num1 + num2;
+	for (i = 3; i <= 98; i++)
+	{
+	if (num11 + num22 > LARGEST || num1 > 0 || num2 > 0)
+	{
+	nonda1 = (num11 + num22) / LARGEST;
+	nonda2 = (num11 + num22) % LARGEST;
+	nonda3 = num1 + num2 + nonda1;
+	num1 = num2, num2 = nonda3;
+	snum11 = num22, num22 = nonda2;
 
+	printf("%lu%010lu", num2, num22);
+	}
+	else
+	{
+	nonda2 = (num11 + num22);
+	num11 = num22, num22 = nonda2;
+
+	printf("%" PRId64 "", spart_num2);
+	}
+
+	if (i != 98)
+	{
+	printf(", ");
+	}
 }
-
 printf("\n");
 
 return (0);
+
 }
-
-
-
