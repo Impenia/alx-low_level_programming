d1627728be8805e306f464cf21e83a810f738658
Impenia <akpaetuki@gmail.com>
2022-09-19 09:59:43 -0700

    Atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,40 +9,39 @@
 * Return: integer converted
 */
 
-int _atoi(char* s)
+int _atoi(char *s)
 {
 
-    int sign = 1, base = 0, i = 0;
-    
-    /* if whitespaces then ignore.*/
-    while (s[i] == ' ')
-    {
-        i++;
-    }
+	int sign = 1, base = 0, i = 0;
 
-    /* sign of number */
-    if (s[i] == '-' || s[i] == '+')
-    {
-        sign = 1 - 2 * (s[i++] == '-');
-    }
+	/* if whitespaces then ignore.*/
+	while (s[i] == ' ')
+	{
+	i++;
+	}
 
-    /* checking for valid input */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
+	/* sign of number */
+	if (s[i] == '-' || s[i] == '+')
+	{
+	sign = 1 - 2 * (s[i++] == '-');
+	}
 
-        /* handling overflow test case */
-        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7))
-        {
-            if (sign == 1)
-                return INT_MAX;
-            else
-                return INT_MIN;
-        }
+	/* checking for valid input */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
 
-        base = 10 * base + (s[i++] - '0');
-    }
+	/* handling overflow test case */
+	if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7))
+	{
+		if (sign == 1)
+		return INT_MAX;
+		else
+		return INT_MIN;
+	}
 
-    return base * sign;
+	base = 10 * base + (s[i++] - '0');
+	}
+
+	return base * sign;
 }
 
- 
