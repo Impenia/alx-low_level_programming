85bdd432e0e27f2815fe8b25f6f40b44ef4a4d3c
Impenia <akpaetuki@gmail.com>
2022-09-19 14:18:47 -0700

    Random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -44,5 +44,29 @@ for(i = 0; i < password_length; i++)
 
 printf("\n");
 
+return (0);
+
 }
+
+
+int main() {
+
+int password_length;
+
+printf("\n\t*********************************\n\n");
+printf("\tWelcome to the password generator\n\n");
+printf("\t*********************************\n");
+printf("\n\tEnter length of the password = ");
+scanf("%d", &password_length);
+
+printf("\n");
+
+printf("\n");
+
+password(password_length);
+
+return 0;
+
+}
+
 
