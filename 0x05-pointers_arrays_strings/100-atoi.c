594e46b318522b4b9b182659f28b7bda619f8ac9
Impenia <akpaetuki@gmail.com>
2022-09-20 13:59:44 -0700

    atoi - convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* _atoi - function to converts a string to integer
-* @s: strint to be converted
-*
-* Return: integer converted
-*/
+ * _atoi - convert a string into an integer.*
+ * @s: the string to convert.
+ *
+ * Return: integer.
+ */
 
 int _atoi(char *s)
 {
+	int sg = 1;
+	int i = 0;
+	unsigned int r = 0;
 
-	int sign = 1, base = 0, i = 0;
-
-	/* if whitespaces then ignore.*/
-	while (s[i] == ' ')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-	i++;
+		if (s[i] == '-')
+			sg *= -1;
+		i++;
 	}
-
-	/* sign of number */
-	if (s[i] == '-' || s[i] == '+')
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
-	sign = 1 - 2 * (s[i++] == '-');
+		r = (r * 10) + (s[i] - '0');
+		i++;
 	}
-
-	/* checking for valid input */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-
-	/* handling overflow test case */
-	if (base > INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7))
-	{
-		if (sign == 1)
-		return (INT_MAX);
-		else
-		return (INT_MIN);
-	}
-
-	base = 10 * base + (s[i++] - '0');
-	}
-
-	return (base * sign);
+	r *= sg;
+	return (r);
 }
-
