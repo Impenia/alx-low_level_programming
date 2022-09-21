a13b620848001924451af740df5b5b241cd2f602
Impenia <akpaetuki@gmail.com>
2022-09-21 11:48:13 -0700

    String Concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,8 +3,8 @@
 
 /**
 * _strcat - a function to concatenate two strings
-* @*dest: first string
-* @*src: second string
+* @dest: first string
+* @src: second string
 *
 * Return: void
 */
@@ -13,15 +13,15 @@
 char *_strcat(char *dest, char *src);
 {
 
-	int i; 
-	int j = _strlen(src); 
+	int i;
+	int j = _strlen(src);
 
-	for (i = 0; src[i] != '\0'; i++) 
-	{ 
-	dest[i + j] = src[i]; 
-	} 
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i + j] = src[i];
+	}
 
-	dest[i + j] = '\0'; 
+	dest[i + j] = '\0';
 
-    return(dest); 
+return (dest);
 }
