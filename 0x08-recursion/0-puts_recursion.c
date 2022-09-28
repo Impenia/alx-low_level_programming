3475de244e76a572c73a1b24cd47606c1de29d24
Impenia <akpaetuki@gmail.com>
2022-09-28 06:13:07 -0700

    Prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _puts_recursion - a function that prints a string 
+* @s: string to be printed
+*
+* Return: (0) Success
+*/
+
+void _puts_recursion(char *s)
+{
+	int i;
+	
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	i++;
+	_puts_recursion(s[i]);
+}
+
