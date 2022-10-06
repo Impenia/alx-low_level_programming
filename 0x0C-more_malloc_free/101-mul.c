01f9c01c1e992e666c47eacb83e2122f3ed84c23
Impenia <akpaetuki@gmail.com>
2022-10-06 14:32:17 -0700

    A program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,32 +1,19 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
- *
- *  * is_digit - checks if a string contains a non-digit char
- *
- *   * @s: string to be evaluated
- *
- *    *
- *
- *     * Return: 0 if a non-digit is found, 1 otherwise
- *
- *      */
+* is_digit - checks if a string contains a non-digit char
+* @s: string to be evaluated
+*
+* Return: 0 if a non-digit is found, 1 otherwise
+*/
 
 int is_digit(char *s)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (s[i])
+	while (s[i])
 
 					{
 
