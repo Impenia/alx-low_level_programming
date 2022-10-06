d5541d0669ec9ca091b7b836cf1fc53f2c2cee2f
Impenia <akpaetuki@gmail.com>
2022-10-06 12:34:30 -0700

    A process that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
 	unsigned int s2_len = strlen(s2);
-	char *s3 = malloc(strlen(s1) + (n + 1));
 
 	if (s3 == NULL)
 		return (NULL);
@@ -26,8 +25,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == NULL)
+	if (s2_len <= 0)
 		s2 = "";
+
+	s3 = malloc(strlen(s1) + (n + 1));
 
 	for (i = 0; i < strlen(s1); i++)
 		s3[i] = s1[i];
