e050522b3fdbf2267b103dc57cfd0739b9e48ec0
Impenia <akpaetuki@gmail.com>
2022-10-06 14:30:16 -0700

    A program that multiplies two positive numbers

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,8 +24,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-		free(ptr);
-		ptr = malloc(new_size);
+	
+	free(ptr);
+	ptr = malloc(new_size);
 
 	return (ptr);
 }
