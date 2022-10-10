9000d876866d2549b993e214687e91fcd421ac76
Impenia <akpaetuki@gmail.com>
2022-10-10 13:16:50 -0700

    Define a new type struct dog

---
+++ 0x0E-structures_typdef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* init_dog - a function that initializes a variable of type struct dog
+* @d: First element
+* @name: Second element
+* @age: Third element
+* @owner: Fourth element
+*
+* Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog
+	{
+		*d;
+		char *name;
+		float age;
+		char *owner;
+	}
