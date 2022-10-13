977d9df5d12a8352f33f02236d7f977a38e205b5
Impenia <akpaetuki@gmail.com>
2022-10-13 04:26:43 -0700

    A function that returns the sum of all its parts

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,10 +14,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum = 0;
 	va_list n_arg;
 
-	va_start(n_arg, n);
-
 	if (n == 0)
 		return (0);
+
+	va_start(n_arg, n);
 
 	for (i = 0; i < n; i++)
 	{
