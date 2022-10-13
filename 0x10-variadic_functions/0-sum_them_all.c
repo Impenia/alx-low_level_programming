8e75b56b485496431817c3c663e58e0e5a29ac46
Impenia <akpaetuki@gmail.com>
2022-10-13 04:19:43 -0700

    A function that returns the sum of all its parts

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* sum_them_all - A function that returns the sum of all its parts
+* @unsigned int: number of arguments.
+* @...: variable arguments
+*
+* Return: sum of arguments
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list n_arg;
+
+	va_start(n_arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(n_arg, unsigned int);
+	}
+	va_end(n_arg);
+
+	return (sum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  8
